2d1a3fb1bda818f11b053d3c75f93ec59b54c5ec
Deepak12041999 <yadavd4631@gmail.com>
2025-03-15 18:41:59 +0530

    error fix

--- src/components/DisplayHome.jsx
+++ src/components/DisplayHome.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Navbar from "./Navbar";
 import { albumsData } from "../assets/assets";
-import AlbumItem from "./Albumitem";
+import AlbumItem from "./AlbumItem";
 import { songsData } from "../assets/assets";
 import SongItem from "./SongItem";
 
